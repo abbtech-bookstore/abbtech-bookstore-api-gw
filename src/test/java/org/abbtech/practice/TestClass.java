fe77d4c4242f03313191d4deee78d7fbf01a3b0f
revan <hrevan983@gmail.com>
2024-05-29 02:42:08 +0400

    i wrote general codes, but i have problem request part for postman, we have to fix that

---
+++ src/test/java/org/abbtech/practice/TestClass.java
@@ -0,0 +1,15 @@
+package org.abbtech.practice;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApgwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
